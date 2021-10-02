b92c15b9fef130088065931123c2e4744a1b797a
Nikita Kishore <kvasir3000@gmail.com>
2021-10-02 15:54:43 +0300

    Display background image

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFML/Window/VideoMode.hpp>
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Rover", sf::Style::Fullscreen);
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
-
+    sf::Texture background;
+    background.loadFromFile("space.jpg");
+    sf::Sprite backgroundSprite(background);
     while (window.isOpen())
     {
         sf::Event event;
@@ -16,6 +20,7 @@ int main()
         }
 
         window.clear();
+        window.draw(backgroundSprite);
         window.draw(shape);
         window.display();
     }
